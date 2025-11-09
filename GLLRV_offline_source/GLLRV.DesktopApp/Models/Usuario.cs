b8e37fcf465add7975cd6ef8cb1c0ce07ce8792f
viniciusbittencout13-cmd <viniciusbittencout.13@gmail.com>
2025-11-09 17:02:31 -0300

    Update Usuario.cs

--- GLLRV_offline_source/GLLRV.DesktopApp/Models/Usuario.cs
+++ GLLRV_offline_source/GLLRV.DesktopApp/Models/Usuario.cs
@@ -1,12 +1,9 @@
-namespace GLLRV.DesktopApp.Models
+public class Usuario
 {
-    public class Usuario
-    {
-        public string Username { get; set; } = "";
-        public string Senha { get; set; } = "";
-        public int Nivel { get; set; }
-        public string Atribuicoes { get; set; } = "";
-        public bool PrimeiroAcesso { get; set; } = true;
-        public string FraseSeguranca { get; set; } = "";
-    }
+    public string Username { get; set; } = "";
+    public string Senha { get; set; } = "";
+    public int Nivel { get; set; }
+    public string Atribuicoes { get; set; } = "";
+    public bool PrimeiroAcesso { get; set; } = true;
+    public string FraseSeguranca { get; set; } = "";
 }
