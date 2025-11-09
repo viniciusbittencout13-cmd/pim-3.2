a769f3ddaef8757e6c0f83012949187f771004e4
viniciusbittencout13-cmd <viniciusbittencout.13@gmail.com>
2025-11-09 18:57:02 -0300

    Update Usuario.cs

--- GLLRV_offline_source/GLLRV.DesktopApp/Models/Usuario.cs
+++ GLLRV_offline_source/GLLRV.DesktopApp/Models/Usuario.cs
@@ -4,22 +4,12 @@ namespace GLLRV.DesktopApp.Models
 {
     public class Usuario
     {
-        // Nome de login
-        public string Username { get; set; } = "";
-
-        // Senha armazenada como SHA256 em hexadecimal
-        public string Senha { get; set; } = "";
-
-        // Nível do técnico (ex: 1, 2, 3)
-        public int Nivel { get; set; }
-
-        // Atribuições / categoria do técnico
-        public string Atribuicoes { get; set; } = "";
-
-        // Se true, obriga passar pela tela de primeiro acesso
-        public bool PrimeiroAcesso { get; set; } = true;
-
-        // Frase de segurança para recuperação de senha
+        public string NomeUsuario { get; set; } = "";
+        public string NomeCompleto { get; set; } = "";
+        public string Nivel { get; set; } = "";
+        public string Categoria { get; set; } = "";
+        public string SenhaHash { get; set; } = "";
         public string FraseSeguranca { get; set; } = "";
+        public bool PrimeiroAcesso { get; set; } = true;
     }
 }
