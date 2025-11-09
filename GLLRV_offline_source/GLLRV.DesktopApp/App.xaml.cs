550bb0a0c4f1cc6323a5ba717732a47e02695408
viniciusbittencout13-cmd <viniciusbittencout.13@gmail.com>
2025-11-09 12:54:13 -0300

    Update App.xaml.cs

--- GLLRV_offline_source/GLLRV.DesktopApp/App.xaml.cs
+++ GLLRV_offline_source/GLLRV.DesktopApp/App.xaml.cs
@@ -1,23 +1,13 @@
-using System;
-using System.IO;
 using System.Windows;
 using GLLRV.DesktopApp.Views;
-using GLLRV.DesktopApp.Services;
-using GLLRV.DesktopApp.Models;
 
 namespace GLLRV.DesktopApp
 {
     public partial class App : Application
     {
-        public static string DataFolderPath =>
-            Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data");
-
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-
-            // Garante que a pasta existe e que há um usuário padrão
-            Auth.CarregarUsuarios(); // se não existir, cria com vinicius/admin
 
             var login = new LoginWindow();
             login.Show();
