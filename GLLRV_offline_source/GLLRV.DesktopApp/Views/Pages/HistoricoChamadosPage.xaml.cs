ffe58d64c38beae694d5a5284e8fde485a9ae5d4
viniciusbittencout13-cmd <viniciusbittencout.13@gmail.com>
2025-11-09 20:30:13 -0300

    Create HistoricoChamadosPage.xaml.cs

---
+++ GLLRV_offline_source/GLLRV.DesktopApp/Views/Pages/HistoricoChamadosPage.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace GLLRV.DesktopApp.Views.Pages
+{
+    public partial class HistoricoChamadosPage : Page
+    {
+        public HistoricoChamadosPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
